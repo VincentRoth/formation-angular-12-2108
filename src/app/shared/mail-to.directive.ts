8746cd035994a9904f2e0976ad7116a3aa3518c6
Vincent Roth <vincent@placeme.io>
2021-08-04 11:32:49 +0200

    Directive avec property binding

--- src/app/shared/mail-to.directive.ts
+++ src/app/shared/mail-to.directive.ts
@@ -1,13 +1,14 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appMailTo]',
 })
 export class MailToDirective implements AfterViewInit {
+  @Input() appMailTo!: string;
+
   constructor(private el: ElementRef<HTMLAnchorElement>) {}
 
   ngAfterViewInit(): void {
-    const email = this.el.nativeElement.innerHTML;
-    this.el.nativeElement.href = 'mailto:' + email;
+    this.el.nativeElement.href = 'mailto:' + this.appMailTo;
   }
 }
