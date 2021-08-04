aed8f537050e46e22414eb248e507f4353c54ef2
Vincent Roth <vincent@placeme.io>
2021-08-04 15:08:20 +0200

    Intercepteur

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AnimalService } from './api/animal.service';
 import { MailToDirective } from './mail-to.directive';
 import { PhoneNumberFormatPipe } from './phone-number-format.pipe';
+import { TimeInterceptor } from './api/time.interceptor';
 
 @NgModule({
-  providers: [AnimalService],
+  providers: [
+    AnimalService,
+    { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true },
+  ],
   declarations: [MailToDirective, PhoneNumberFormatPipe],
   imports: [HttpClientModule],
   exports: [MailToDirective, PhoneNumberFormatPipe],
