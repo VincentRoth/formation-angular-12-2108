c23f8e634152372bff28aa7caa06f49b36782918
Vincent Roth <vincent@placeme.io>
2021-08-04 17:28:28 +0200

    Route asynchrone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', redirectTo: 'animals', pathMatch: 'full' }];
+const routes: Routes = [
+  { path: '', redirectTo: 'animals', pathMatch: 'full' },
+  {
+    path: 'vet',
+    loadChildren: () =>
+      import('./vet/vet.module').then(
+        (moduleTypeScript) => moduleTypeScript.VetModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
